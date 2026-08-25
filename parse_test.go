054dda8b0cd8b70b6c4e8f58597b633b06cc6fda
da123wda <133342551+da123wda@users.noreply.github.com>
2026-08-25 15:36:21 +0800

    Initial import: snowflake ID generator

---
+++ parse_test.go
@@ -0,0 +1,24 @@
+package id
+
+import (
+	"errors"
+	"testing"
+	"time"
+)
+
+func TestParseRejectsNegativeID(t *testing.T) {
+	_, _, _, err := parseValue(-1)
+	if !errors.Is(err, ErrInvalidID) {
+		t.Fatalf("Parse(-1) error = %v, want ErrInvalidID", err)
+	}
+}
+
+func TestParseUsesCustomEpoch(t *testing.T) {
+	timestamp, machineID, sequence, err := parseValue(0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !timestamp.Equal(time.UnixMilli(EpochMilliseconds)) || machineID != 0 || sequence != 0 {
+		t.Fatalf("Parse(0) = (%v, %d, %d), want custom epoch and zero fields", timestamp, machineID, sequence)
+	}
+}
